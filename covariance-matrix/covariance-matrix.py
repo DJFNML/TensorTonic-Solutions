6f5e021ddb048bf00946d66081a3bd9e5efaca2a
Dylan Ng <ndylang@gmail.com>
2026-09-23 15:22:45 +0100

    Solve covariance-matrix

---
+++ covariance-matrix/covariance-matrix.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def matrix_transpose(A: list) -> np.ndarray:
+    """
+    Returns the transposed matrix as a NumPy array.
+    """
+    # Write code here
+    B = np.zeros((len(A[0]), len(A)))
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            B[j][i] = A[i][j]
+
+    return B
+
+def covariance_matrix(X: list) -> np.ndarray:
+    """
+    Returns the covariance matrix as a NumPy array.
+    """
+    # Write code here
+    
+    Xc = X - np.mean(X,axis = 0)
+    sig = np.matmul(matrix_transpose(list(Xc)), Xc) /(len(Xc)-1)
+    return sig
